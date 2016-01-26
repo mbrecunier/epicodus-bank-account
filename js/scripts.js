64105a14e1bac9c0a508e0df09854bed17abf27f
Josh Gustafson and Michelle Brecunier <silversevenfox@yahoo.com>
2016-01-26 15:21:41 -0800

    write withdraw method

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ function BankAccount(name, initialBalance) {
   this.name = name;
   this.balance = initialBalance;
 }
+
+BankAccount.prototype.withdraw = function(amount) {
+  return this.balance -= amount;
+}
