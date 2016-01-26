0e299247ffcf2e7e52bfcbfbae23a35bfa904556
Josh Gustafson and Michelle Brecunier <silversevenfox@yahoo.com>
2016-01-26 15:14:22 -0800

    write spec for withdraw method

--- spec/specs.js
+++ spec/specs.js
@@ -4,4 +4,8 @@ describe("BankAccount", function() {
     expect(testBankAccount.name).to.equal("Mike");
     expect(testBankAccount.balance).to.equal(500);
   });
+  it("will add the withdraw method to BankAccount objects", function() {
+    var testBankAccount = new BankAccount("Mike", 500);
+    expect(testBankAccount.withdraw(300)).to.equal(200);
+  })
 });
