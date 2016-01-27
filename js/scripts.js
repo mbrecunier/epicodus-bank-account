482af1028a6060fd7cab38d340b823bf68a59181
Josh Gustafson and Michelle Brecunier <silversevenfox@yahoo.com>
2016-01-26 16:07:18 -0800

    append functionless account interface to createdAccounts div

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business Logic
 function BankAccount(name, initialBalance) {
   this.name = name;
   this.balance = initialBalance;
@@ -10,3 +11,33 @@ BankAccount.prototype.withdraw = function(amount) {
 BankAccount.prototype.deposit = function(amount) {
   return this.balance += amount;
 }
+
+// User Interface Logic
+var createBankAccountUI = function(account) {
+  $('div#createdAccounts').append('<div class="col-md-3">' +
+                                    '<h3>' + account.name + '</h3>' +
+                                    '<form id="accountInterface">' +
+                                      '<div class="form-group">' +
+                                        '<label>Withdraw</label>' +
+                                        '<input id="withdrawAmount" class="form-control" type="number">' +
+                                      '</div>' +
+                                      '<div class="form-group">' +
+                                        '<label>Deposit</label>' +
+                                        '<input id="depositAmount" class="form-control" type="number">' +
+                                      '</div>' +
+                                      '<button class="btn btn-primary" type="submit">Get new balance</button>' +
+                                    '</form>' +
+                                  '</div>');
+}
+
+$(function() {
+  $("form#createNewAccount").submit(function(event) {
+    event.preventDefault();
+
+    var accountName = $("input#accountName").val();
+    var initialDeposit = $("input#initialDeposit").val();
+    var newBankAccount = new BankAccount(accountName, initialDeposit);
+
+    createBankAccountUI(newBankAccount);
+  });
+});
